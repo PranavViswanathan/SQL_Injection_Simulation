9fc12b7b01edf014e2294e5a0488f036b51983a3
Pranav Viswanathan <72350161+PranavViswanathan@users.noreply.github.com>
2022-01-09 13:55:15 +0530

    Create myDb.sql

---
+++ DatabaseSetup/myDb.sql
@@ -0,0 +1,13 @@
+create table myDB (
+	id int,
+    fname char(100),
+    lname char(100)
+);
+SELECT * FROM myDB;
+Select * from myDB where id = 1;
+INSERT INTO myDB VALUES (1, 'Pranav', 'Viswanathan');
+INSERT INTO myDB VALUES (2, 'ranav', 'q');
+INSERT INTO myDB VALUES (3, 'anav', 'w');
+INSERT INTO myDB VALUES (4, 'Pranav', 'catch1');
+INSERT INTO myDB VALUES (5, 'Pranav', 'catch2');
+INSERT INTO myDB VALUES (6, 'Pranav', 'catch3');
